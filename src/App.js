be46d08233e1bcf8b669004043182050ad4715e5
Yasin <yasinalbayrak@sabanciuniv.edu>
2023-09-02 03:48:20 +0300

    commit9

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ChampionsLeague from './Components/ChampionsLeagueComponents/ChampionsLea
 
 function App() {
   return (
-    <BrowserRouter basename="/yasinalbayrak/uefaLeagues">
+    <BrowserRouter basename="/yasinalbayrak.github.io/uefaLeagues/">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/ChampionsLeague" element={<ChampionsLeague />} />
