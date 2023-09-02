b3581d381d338b4145ecf76068e43085834d5f7f
Yasin <yasinalbayrak@sabanciuniv.edu>
2023-09-02 21:07:41 +0300

    commit20

--- src/Components/NavigationBarComponents/Navbar.jsx
+++ src/Components/NavigationBarComponents/Navbar.jsx
@@ -1,51 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Button } from 'react-bootstrap'; // Import React Bootstrap components
+
 import uefaLogo from '../../images/uefa.png';
 import { useTranslation } from 'react-i18next';
 import LanguageSelector from './LanguageSelector/LanguageSelector';
-import conferenceLogo from "../../images/conference2.png"
-function Navbar() {
-    const { t } = useTranslation();
-    const navigate = useNavigate()
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <div className="container">
-                <a className="navbar-brand" onClick={()=>{navigate("/")}}>
-                    <img className='uefa-logo' src={uefaLogo} alt="Your Logo" />
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link cl" onClick={()=>{navigate("/europe?league=cl")}}>
-                                {t('CL')}
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link el" onClick={()=>{navigate("/europe?league=el")}}>{t('EL')}</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link ecl" onClick={()=>{navigate("/europe?league=ecl")}}>{t('ECL')}</a>
-                        </li>
+import conferenceLogo from "../../images/conference2.png";
 
-                    </ul>
+function CustomNavbar() {
+    const [expanded, setExpanded] = useState(false);
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+
+    const handleNavCollapse = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Navbar expand="lg" bg="dark" variant="dark">
+            <div className="container">
+                <Navbar.Brand  onClick={() => navigate("/")}>
+                    <img className='uefa-logo' src={uefaLogo} alt="Your Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle onClick={handleNavCollapse} aria-controls="navbarNav" />
+                <Navbar.Collapse id="navbarNav" className={`justify-content-end ${expanded ? 'show' : ''}`}>
+                    <Nav className="mr-auto">
+                        <Nav.Link className='navl-ink cl' onClick={() => navigate("/europe?league=cl")}>{t('CL')}</Nav.Link>
+                        <Nav.Link className='navl-ink el' onClick={() => navigate("/europe?league=el")}>{t('EL')}</Nav.Link>
+                        <Nav.Link className='navl-ink ecl' onClick={() => navigate("/europe?league=ecl")}>{t('ECL')}</Nav.Link>
+                    </Nav>
                     <LanguageSelector />
-                </div>
+                </Navbar.Collapse>
             </div>
-        </nav>
+        </Navbar>
     );
 }
 
-export default Navbar;
+export default CustomNavbar;
